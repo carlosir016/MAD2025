f07f4f59d3cb37764d5bcdbe833d8af1f589400d
Carlos <c.ybanez.ruiz@alumnos.upm.es>
2025-04-03 15:12:13 +0200

    Added image, pushing to work on main computer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation ("androidx.recyclerview:recyclerview:1.3.0")
+
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    kapt("com.github.bumptech.glide:compiler:4.15.1")
+
 }
